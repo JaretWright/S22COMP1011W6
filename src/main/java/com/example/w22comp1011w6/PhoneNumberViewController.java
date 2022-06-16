fcf5621f631131eab6a98ef3bf0207de6f67906c
JaretWright <jaret.wright@georgiancollege.ca>
2022-06-16 09:55:15 -0400

    Added logic to validate a basic 10 digit phone number

--- src/main/java/com/example/w22comp1011w6/PhoneNumberViewController.java
+++ src/main/java/com/example/w22comp1011w6/PhoneNumberViewController.java
@@ -1,4 +1,35 @@
 package com.example.w22comp1011w6;
 
-public class PhoneNumberViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PhoneNumberViewController implements Initializable {
+
+    @FXML
+    private Label areaCodeLabel;
+
+    @FXML
+    private Label cityCodeLabel;
+
+    @FXML
+    private Label phoneNumLabel;
+
+    @FXML
+    private TextField phoneNumTextField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        phoneNumTextField.textProperty().addListener((obs, oldValue, phoneNum) -> {
+            if (phoneNum.matches("[2-9]\\d{2}[2-9]\\d{2}\\d{4}"))
+                phoneNumLabel.setText("Valid phone number");
+            else
+                phoneNumLabel.setText("Invalid phone number");
+
+        });
+    }
 }
